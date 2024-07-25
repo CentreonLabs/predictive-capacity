50c307464eb511c44ce9f348a6a41af94e459c90
Denis Roussel <droussel@centreon.com>
2024-07-25 14:50:17 -0400

    Move fixture envronment variable to conftest

--- backend/tests/test_upload.py
+++ backend/tests/test_upload.py
@@ -1,18 +1,8 @@
 import json
-import os
 
 import boto3
 import pytest
 from moto import mock_aws
-
-
-@pytest.fixture
-def aws_credentials(scope="session", autouse=True):
-    """Mocked AWS Credentials for moto."""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
 
 
 @mock_aws
